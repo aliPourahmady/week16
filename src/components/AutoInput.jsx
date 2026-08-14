d64ced7c1a7c82701cbf5d179531ac0b4d142704
ali pourahmady <alipourahmady@gmail.com>
2026-08-14 18:07:24 +0330

    modified:   src/components/AutoInput.jsx

--- src/components/AutoInput.jsx
+++ src/components/AutoInput.jsx
@@ -1,8 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function AutoInput() {
-  const [text, setText] = useState("");
-  return <div>hello</div>;
+  const [query, setQuery] = useState("");
+  cosnt[(suggestion, setSuggestion)] = useState("");
+
+  useEffect(() => {
+    const getData = async () => {
+      const res = await fetch("../cities.json");
+      const json = await res.json();
+      console.log(json);
+    };
+    getData();
+  }, []);
+  const autoCompleteHandler = (e) => {
+    setQuery(e.target.value);
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder={suggestion}
+        value={query}
+        onChange={autoCompleteHandler}
+      />
+    </div>
+  );
 }
 
 export default AutoInput;
